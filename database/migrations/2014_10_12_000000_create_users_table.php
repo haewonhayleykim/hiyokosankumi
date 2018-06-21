feb6346420d198dd6e711f864420bf6961fd7dc0
jiro.kirameki <ec2-user@ip-172-31-43-201.us-east-2.compute.internal>
2018-06-21 06:13:26 +0000

    git

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,11 +17,12 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
